712319d6ece008e79a524c652134f39904f56ddb
stella-maris <mango.gal@gmail.com>
2017-09-17 20:10:31 -0700

    Adds click event for tabs

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
-var twitterAPI = require('twit');
-var twitter = new twitterAPI({
-    consumer_key: 'JLcyUcHBGYUVtTqGouwJoPoWG',
-    consumer_secret: 'CCJHMTWlMqOLsQMyGI9MWwv3N0DGtPSYRibu4RWjO1xJh54kAG',
-    access_token: '45609961-NEwKfiI0pAFbkQKmij3PCHdYnGVULtpCABFvFAxhF',
-    access_token_secret: 'BFoUHxuJtJCI26sX7X9Y9MguPpiDE4AKFBXPGG8illnOI'
+//var twitterAPI = require('twit');
+//var twitter = new twitterAPI({
+//    consumer_key: 'JLcyUcHBGYUVtTqGouwJoPoWG',
+//    consumer_secret: 'CCJHMTWlMqOLsQMyGI9MWwv3N0DGtPSYRibu4RWjO1xJh54kAG',
+//    access_token: '45609961-NEwKfiI0pAFbkQKmij3PCHdYnGVULtpCABFvFAxhF',
+//    access_token_secret: 'BFoUHxuJtJCI26sX7X9Y9MguPpiDE4AKFBXPGG8illnOI'
+//});
+//
+//
+//var params = {
+//    q: 'love',
+//    count: 100
+//}
+//
+//twitter.get('search/tweets', params,searchedData);
+//
+//function searchedData(err, data, response) {
+//    console.log(data);
+//    for (var i = 0; i < data.length; i++) {
+//        var data = data[i];
+//        $('body').append('<h3>' + data.screen_name + '</h3>' + '<p>'+ data.name + '</p>');
+//    }   
+//}
+
+$('.nav-item').click(function () {
+    $('.nav-item').removeClass('active');
+    $(this).addClass('active');
 });
 
-
-var params = {
-    q: 'love',
-    count: 100
-}
-
-twitter.get('search/tweets', params,searchedData);
-
-function searchedData(err, data, response) {
-    console.log(data);
-    for (var i = 0; i < data.length; i++) {
-        var data = data[i];
-        $('body').append('<h3>' + data.screen_name + '</h3>' + '<p>'+ data.name + '</p>');
-    }   
-}
+$('.dashboard-tab').click(function () {
+    $('.dashboard-tab').removeClass('active');
+    $(this).addClass('active');
+})
